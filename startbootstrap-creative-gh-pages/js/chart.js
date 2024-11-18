7edd617aa6d1f47668b749f5ef3f15ea7458da8d
Marius Servais <s1069585@stud.sbg.ac.at>
2024-11-18 18:01:48 +0100

    first commit

---
+++ startbootstrap-creative-gh-pages/js/chart.js
@@ -0,0 +1,69 @@
+// <block:setup>
+const data = {
+    labels: [
+      'Data Aquisition',
+      'Data Engineering',
+      'Data Analysis',
+      'Data Science',
+      'Software Development'
+    ],
+    
+    datasets: [{
+      label: 'Profile',
+      data: [65, 80, 85, 85, 65],
+      fill: true,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgb(255, 99, 132)',
+      pointBackgroundColor: 'rgb(255, 99, 132)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgb(255, 99, 132)'
+    }]
+  };
+  // </block:setup>
+  
+  // <block:config:0>
+  const config = {
+    type: 'radar',
+    data: data,
+    options: {
+      elements: {
+        line: {
+          borderWidth: 3
+        }
+      },
+      scales: {
+        r: {
+            angleLines: {
+                display: true
+            },
+            suggestedMin: 0,
+            suggestedMax: 100,
+            pointLabels: {
+                font: {
+                    size: 16
+                }
+            },
+            ticks: {
+                font:{
+                    size:10
+                }
+            }
+            
+        }       
+    },
+    plugins: {
+        legend: {
+            labels: {
+                // This more specific font property overrides the global property
+                font: {
+                    size: 20
+                }
+            }
+        }
+    }
+    },
+  };
+  // </block:config>
+  
+
